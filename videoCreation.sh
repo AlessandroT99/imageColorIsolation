b8c3d3cca2be96801b9932b1d3dab0437c2f987f
Alessandro Tiozzo <62152778+AlessandroT99@users.noreply.github.com>
2024-02-22 14:30:33 +0100

    Added check on folders
    - added check on folders over 9

--- videoCreation.sh
+++ videoCreation.sh
@@ -13,7 +13,7 @@ echo "Video creation for all the $numdirs tests"
 for cnt in {1..9}
 do
 	cd "./P_0000"$cnt
-	printf "\nConverting files for test number $cnt... "
+	printf "\nConverting files for test number $cnt...  "
 	if ! [ -f *.mp4 ]; then
 		ffmpeg -framerate 30 -i "%08d.ppm" -vcodec libx264 -crf 25 -pix_fmt yuv420p "P$cnt.mp4"
 		echo "Conversion for test number $cnt completed."
@@ -23,7 +23,7 @@ do
 	cd ..
 done
 
-if [$numdirs > 9]; then 
+if (($numdirs > 9)); then 
 	for cnt in {10..23}
 	do
 		cd "./P_000"$cnt
