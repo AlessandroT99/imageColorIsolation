e1417b4b61a4a4f41c4d1a6c4ac97c9c1d0ed7fe
Alessandro Tiozzo <62152778+AlessandroT99@users.noreply.github.com>
2024-02-22 14:44:07 +0100

    Update videoCreation.sh
    quick fix

--- videoCreation.sh
+++ videoCreation.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#nTest=ls | wc -l
-#((nTest=nTest-2))
 # Counting number of directories
 shopt -s nullglob
 numdirs=(*/)
@@ -12,25 +10,27 @@ echo "Video creation for all the $numdirs tests"
 
 for cnt in {1..9}
 do
-	cd "./P_0000"$cnt
-	printf "\nConverting files for test number $cnt...  "
-	if ! [ -f *.mp4 ]; then
+	cd "./P_0000$cnt"
+	printf "\nConverting files for test number 0$cnt... "
+	if ! [ -f "P$cnt.mp4" ]; then
+		echo ""
 		ffmpeg -framerate 30 -i "%08d.ppm" -vcodec libx264 -crf 25 -pix_fmt yuv420p "P$cnt.mp4"
-		echo "Conversion for test number $cnt completed."
+		printf "\nConversion for test number 0$cnt completed."
 	else
-		echo "File for test number $cnt already created."
+		echo "File for test number 0$cnt already created."
 	fi
 	cd ..
 done
 
 if (($numdirs > 9)); then 
-	for cnt in {10..23}
+	for (( cnt=10; cnt<=$numdirs; cnt++ ))
 	do
-		cd "./P_000"$cnt
+		cd "./P_000$cnt"
 		printf "\nConverting files for test number $cnt... "
-		if ! [ -f *.mp4 ]; then
+		if ! [ -f "P$cnt.mp4" ]; then
+			echo ""
 			ffmpeg -framerate 30 -i "%08d.ppm" -vcodec libx264 -crf 25 -pix_fmt yuv420p "P$cnt.mp4"
-			echo "Conversion for test number $cnt completed."
+			printf "\nConversion for test number $cnt completed."
 		else
 			echo "File for test number $cnt already created."
 		fi
@@ -38,5 +38,4 @@ if (($numdirs > 9)); then
 	done
 fi
 
-echo "Video creation complete."
-
+printf "\nVideo creation complete."
